7427947008adedd6fe6fd1fc51aec380123175df
Brianna Rodriguez <brianna.rodriguez2240@gmail.com>
2022-12-09 23:23:48 -0500

    changing background of application to always being black (might change to a lighter dark mode)

--- webifyv2/src/App.jsx
+++ webifyv2/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import { Home, Landing } from './pages'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+          <Route path='/' element={<Landing/>}/>
+          <Route path='/home' element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
