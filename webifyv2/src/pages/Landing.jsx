b608e2d094385963ef42274759bd28b6e717e4de
Brianna Rodriguez <brianna.rodriguez2240@gmail.com>
2022-12-12 13:32:55 -0500

    added a typing animation for the title and animation for the button so that it looks minimalistic yet appeasing to users

--- webifyv2/src/pages/Landing.jsx
+++ webifyv2/src/pages/Landing.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { spotifyAuth } from '../actions/auth';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import Typed from 'react-typed';
 
 const Landing = () => {
     const [ token, setToken ] = useState('');
@@ -14,18 +16,67 @@ const Landing = () => {
     }, []);
 
     const clicked = (e) => {
-        console.log("button clicked");
         e.preventDefault();
         spotifyAuth();
     }
   return (
     <>
-        <div>landing</div>
-        <button onClick={e => clicked(e)}>login</button>
+     <Wrapper>
+        <Title><Typed strings={["Get Great Artist Recommendations", "with", "Webify"]} typeSpeed={50} backSpeed={50} /></Title>
+        <Button onClick={e => clicked(e)}>login</Button>
         { !token ? navigate('/') : navigate('/home')}
+      </Wrapper>
     </>
   )
 }
+
+const Button = styled.button`
+      width: 6rem;
+      height: 3rem;
+      background-color: #1DB954;
+      color: white;
+      border-radius: 10px;
+      font-size: 1rem;
+      cursor: pointer;
+      font-family: "GothamRounded-Medium";
+      transition: all 500ms ease;
+      border: none;
+
+      &:hover {
+        cursor: pointer;
+        color: #1DB954;
+        border: #1DB954 solid 2px;
+        background: transparent;
+      }
+`;
+
+const Wrapper = styled.div`
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      height: 100%;
+      align-items: center;
+      justify-content: center;
+
+`;
+
+const Title = styled.h1`
+
+    color: white;
+    font-family: "GothamRounded-Bold";
+    font-size: 3rem;
+    text-align: center;
+
+  @keyframes blink {
+    0% {
+      opacity: 100%;
+    }
+
+    50% {
+      opacity: 0%;
+    }
+  }
+`;
 
 
 export default Landing;
